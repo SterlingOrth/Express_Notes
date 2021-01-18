d5f72bf209d95493157f5e9eee131f457a70b6c3
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2021-01-17 18:22:06 -0700

    fixed delete route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = process.env.PORT || 3606;
 
@@ -25,8 +26,10 @@ app.get("/notes", (req, res) =>
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
   fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
     console.log(data);
     const db = JSON.parse(data);
+    newNote.id = uuidv4();
     db.push(newNote);
     fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
       res.json(db);
@@ -34,6 +37,25 @@ app.post("/api/notes", (req, res) => {
   });
 });
 
+app.delete("/api/notes/:id", (req, res) => {
+  const noteID = req.params.id;
+  console.log(typeof noteID);
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    let newData = JSON.parse(data).filter((note) => {
+      if (note.id === noteID) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    console.log(newData);
+    fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
+      if (err) throw err;
+      res.json(newData);
+    });
+  });
+});
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
